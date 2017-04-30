7e85aae89505aef56a4ba3f569d520c69b28bd6b
ismaelocaramelo <ismaelbacha@hotmail.com>
2017-04-30 02:16:04 +0100

    Adding routes and controller, request-ip middleware needed

--- config/routes.js
+++ config/routes.js
@@ -1,4 +1,20 @@
-const router  = require('express').Router();
+const router     = require('express').Router();
+const clients    = require('../controllers/clients');
+const path       = require('path');
+
+// ADD A MIDDLEWARE TO GET THE CLIENT IP with request-ip
+
+router.route('/')
+  .get( (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+
+//Client Sign-up
+router.route('/sign_up')
+  .post(clients.signUp)
+
+//Client Info
+router.route('/accounting_platforms')
+  .get(clients.getClientInfo)
+
 
 
 module.exports = router;
---
+++ controllers/clients.js
@@ -0,0 +1,27 @@
+module.exports = {
+  signUp: clientsCreate,
+  getClientInfo: clientsGetInfo
+};
+
+const Client = require('../models/client');
+const bcrypt  = require('bcrypt');
+
+function clientsGetInfo(req, res){
+  Client.find(req.body.email, (err, client) => {
+    if (err) return res.status(500).json(err);
+    if (!client) return res.status(404).json({ error: 'No client was found.' });
+    return res.status(200).json(client);
+  });
+}
+
+function clientsCreate(req, res){
+  Client.create(req.body.client, (err, client) => {
+    if (err) return res.status(500).json(err);
+    user['clientIp'] = // Somehow needs to be stored
+    user.save((err, user) => {
+      if (err) return res.status(status).json({success: false});
+      return res.status(201).json({success: true, message: `Welcome ${client.name}!`, user});
+    });
+  });
+  });
+}
